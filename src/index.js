b0b2b93faa4b306a6162c077262ba02a8a7f94eb
gregoriB <brandon.gregori@gmail.com>
2019-02-15 19:07:17 -0800

    added BrowserRouter wrapper to app in index.js to allow from manual URL typing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { StateProvider } from './contexts/stateContext'
+import { StateProvider } from './contexts/stateContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
     <StateProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StateProvider>,
   document.getElementById('root')
 );
